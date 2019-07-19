2b9181f4bc8b4e0b98d4016c29c3cd567c602815
Jakub Plesník <jakub.plesnik@microton.cz>
2019-07-19 10:49:31 +0200

    Cleanup

--- InvoiceAssignment/Controllers/InvoicesController.cs
+++ InvoiceAssignment/Controllers/InvoicesController.cs
@@ -45,15 +45,17 @@ namespace InvoiceAssignment.Controllers
         }
 
         /// <summary>
-        /// Returns the specified invoice
+        /// Pay the specified invoice
         /// </summary>
         /// <param name="invoiceId">Invoice id</param>
         /// <response code="200">The request was processed successfully</response>
+        /// <response code="400">Invoice was already paid</response>
         /// <response code="404">Invoice was not found</response>
-        [HttpGet("{invoiceId}")]
+        [HttpPost("{invoiceId}/pay")]
         [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        public IActionResult GetInvoice(int invoiceId)
+        public async Task<IActionResult> PayInvoice(int invoiceId)
         {
             var invoice = _dbContext.GetInvoice(invoiceId);
 
@@ -62,38 +64,16 @@ namespace InvoiceAssignment.Controllers
                 return NotFound(new BaseResponse(404, "Invoice was not found."));
             }
 
-            var result = new InvoiceViewModel(invoice);
-
-            return Ok(result);
-        }
-
-        /// <summary>
-        /// Adds a new invoice
-        /// </summary>
-        /// <param name="inv">Invoice body</param>
-        /// <response code="200">The request was processed successfully</response>
-        /// <response code="400">Invalid model state</response>
-        [HttpPost]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        public async Task<IActionResult> AddInvoice([FromBody]Invoice inv)
-        {
-            inv.Id = 0;
-            inv.CreationDate = DateTime.UtcNow;
-            inv.RefNumber = DateTime.Now.ToString("yyyyMMddHHmmss");
-            inv.IsPaid = false;
-
-            if (!ModelState.IsValid)
+            if (invoice.IsPaid)
             {
-                return BadRequest(ModelState);
+                return BadRequest(new BaseResponse(400, "Invoice is already paid."));
             }
 
-            await _dbContext.Invoices.AddAsync(inv);
+            invoice.IsPaid = true;
+
             await _dbContext.SaveChangesAsync();
 
-            var result = new InvoiceViewModel(inv);
-
-            return Ok(result);
+            return Ok(new BaseResponse(200, "Invoice was successfuly paid."));
         }
 
         /// <summary>
@@ -134,106 +114,6 @@ namespace InvoiceAssignment.Controllers
             var result = new InvoiceViewModel(invoice);
 
             return Ok(result);
-        }
-
-        /// <summary>
-        /// Pay the specified invoice
-        /// </summary>
-        /// <param name="invoiceId">Invoice id</param>
-        /// <response code="200">The request was processed successfully</response>
-        /// <response code="400">Invoice was already paid</response>
-        /// <response code="404">Invoice was not found</response>
-        [HttpPost("{invoiceId}/pay")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> PayInvoice(int invoiceId)
-        {
-            var invoice = _dbContext.GetInvoice(invoiceId);
-
-            if (invoice == null)
-            {
-                return NotFound(new BaseResponse(404, "Invoice was not found."));
-            }
-
-            if (invoice.IsPaid)
-            {
-                return BadRequest(new BaseResponse(400, "Invoice is already paid."));
-            }
-
-            invoice.IsPaid = true;
-
-            await _dbContext.SaveChangesAsync();
-
-            return Ok(new BaseResponse(200, "Invoice was successfuly paid."));
-        }
-
-        /// <summary>
-        /// Adds an invoice item to the specified invoice
-        /// </summary>
-        /// <param name="invoiceId">Invoice id</param>
-        /// <param name="item">Invoice item body</param>
-        /// <response code="200">The request was processed successfully</response>
-        /// <response code="400">Invalid model state</response>
-        /// <response code="404">Invoice was not found</response>
-        [HttpPost("{invoiceId}/items")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> AddInvoiceItem(int invoiceId, [FromBody]InvoiceItem item)
-        {
-            var invoice = await _dbContext.Invoices.FindAsync(invoiceId);
-
-            if (invoice == null)
-            {
-                return NotFound(new BaseResponse(404, "Invoice was not found"));
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            item.Invoice = invoice;
-
-            await _dbContext.InvoiceItems.AddAsync(item);
-            await _dbContext.SaveChangesAsync();
-
-            var result = new InvoiceItemViewModel(item);
-
-            return Ok(result);
-        }
-
-        /// <summary>
-        /// Deletes the specified invoice item of the specified invoice
-        /// </summary>
-        /// <param name="invoiceId">Invoice id</param>
-        /// <param name="itemId">Invoice item id</param>
-        /// <response code="200">The request was processed successfully</response>
-        /// <response code="404">Invoice or invoice item was not found</response>
-        [HttpDelete("{invoiceId}/items/{itemId}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
-        public async Task<IActionResult> RemoveInvoiceItem(int invoiceId, int itemId)
-        {
-            var invoice = await _dbContext.Invoices.FindAsync(invoiceId);
-
-            if (invoice == null)
-            {
-                return NotFound(new BaseResponse(404, "Invoice was not found."));
-            }
-
-            var item = await _dbContext.InvoiceItems.FindAsync(itemId);
-
-            if (item == null || item.Invoice != invoice)
-            {
-                return NotFound(new BaseResponse(404, "Item of specified invoice was not found."));
-            }
-
-            _dbContext.InvoiceItems.Remove(item);
-            await _dbContext.SaveChangesAsync();
-
-            return Ok(new BaseResponse(200, "Item was successfuly removed."));
         }
     }
 }
